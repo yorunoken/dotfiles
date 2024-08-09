ac0a829205a7c1c032354e4e8d50d77dd90be9c1
Yoru <m.f.fetvaci@hotmail.com>
2024-08-10 00:04:01 +0300

    [hypr] some stuff

--- config/hypr/scripts/gpu_recorder.sh
+++ config/hypr/scripts/gpu_recorder.sh
@@ -1,3 +1,9 @@
 kill $(pgrep -f gpu-screen-recorder)
 
-gpu-screen-recorder -w DP-2 -c mp4 -f 60 -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -o ~/Videos/AMD -r 300 &
+# You may find this using `pactl get-default-sink`
+default_output="alsa_output.pci-0000_0b_00.4.analog-stereo"
+
+# You may find this using `pactl get-default-source`
+default_input="alsa_input.pci-0000_0b_00.4.analog-stereo"
+
+gpu-screen-recorder -w DP-2 -c mp4 -f 60 -a "$default_output.monitor|$default_input" -o ~/Videos/AMD -r 300 &
